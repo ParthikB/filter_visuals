0f8d9f52fd64282a9486c61a2eb0cf6a97b01d95
Parthik Bhandari <connect2parthik@gmail.com>
2019-12-13 00:31:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,62 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from tqdm import tqdm
+print()
 
 def output_dim(input_size, filter_size, stride):
 	dim = (input_size-filter_size)/stride + 1
 	return int(dim)
+
+
+def input_filter_val(f):
+	try:
+		val = int(input(f'f{f+1} : '))
+	except:
+		print('Invalid Val. Try Again..')
+		val = input_filter_val(f)
+	return val
+
 
 # Importing the Image
 img = cv2.imread('test1.jpg', 0)
 # img = cv2.resize(img, (100, 100)) # Resizing temporarily for faster calculations
 
 # PARAMETERS
-FILTER_SIZE = 3
-STRIDE = 1
+print('------- PARAMETERS -------')
+STRIDE      = int(input('Enter Stride      : '))
+FILTER_SIZE = int(input('Enter Filter Size : '))
 img_row, img_col = img.shape
+
+print()
+print('---- Filter Variables ----')
+for i in range(FILTER_SIZE):
+  for j in range(FILTER_SIZE):
+    print(f' f{(i*3)+j+1} ', end='')
+  print()
+
+
+print()
+print('--- Input Filter Values ---')
+# Filter
+filter_vals = []
+
+for i in range(FILTER_SIZE**2):
+	val = input_filter_val(i)
+	filter_vals.append(val)
+
+print()
 
 output_row_dim = output_dim(img_row, FILTER_SIZE, STRIDE)
 output_col_dim = output_dim(img_col, FILTER_SIZE, STRIDE)
 
 total_pixels = output_row_dim * output_col_dim
 
-# Filter
-filter_vals = [1,  1,  1, 
-			   0,  0,  0, 
-			  -1, -1, -1]
 
 output = []
-
+print('Processing Image...')
+start = time.time()
 for i in tqdm(range(0, img_row-FILTER_SIZE+1, STRIDE)):
 	rows = [i+x for x in range(FILTER_SIZE)]
 
@@ -49,6 +78,7 @@ for i in tqdm(range(0, img_row-FILTER_SIZE+1, STRIDE)):
 output = np.array(output)
 output_img = output.reshape(output_row_dim, output_col_dim)
 
+print(f'Time Taken : {round(time.time()-start, 2)}s')
 
 ax1 = plt.subplot(1, 2, 1)
 ax1.imshow(img, cmap='gray')
